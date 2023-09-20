8446afeae6df855caa2462c5443e743bf6d44430
EstebanIbarra <sectorjeat@gmail.com>
2023-09-19 23:13:54 -0600

    Imports Plugins

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod plugins {
+    pub mod camera;
+    pub mod player;
+    pub mod world;
+}
+
+use bevy::prelude::*;
+use plugins::camera::CameraPlugin;
+use plugins::player::PlayerPlugin;
+use plugins::world::WorldPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            CameraPlugin,
+            PlayerPlugin,
+            WorldPlugin
+        ))
+        .run();
 }
